968e063206139e777ee33318fa5b9fe1be7eb57e
Taha Khan <mohdtahakhan13@gmail.com>
2025-07-04 22:47:32 +0530

    commit final

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -32,7 +32,7 @@ function App() {
 
   async function reviewCode() {
   try {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code });
+    const response = await axios.post('https://code-review-main-backend.onrender.com/ai/get-review', { code });
     setReview(response.data); // Now stores reviewText, score, metrics
   } catch (err) {
     console.error("Error generating review:", err);
